556ea6ec4c97123b3bf01616f9d60f06f1017120
kwong991 <kgwong991@gmail.com>
2018-08-08 15:21:26 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -37,6 +37,7 @@ loop do
   
 end
 a_few_more_steps
+
 def how_many_steps?
   # Write a loop that fulfills all criteria from Part 3 of the README.md
   #steps = 0 
@@ -62,7 +63,7 @@ def how_many_steps?
 # else 
 #   puts "Right"
 # sleep (0.5)
-end
+
 
 
 
